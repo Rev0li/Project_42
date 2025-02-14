d247666329889be74c83694a9eb45588fbb03231
Oliver Kientzler <okientzl@z1r10p1.42lyon.fr>
2025-02-14 08:51:08 +0100

    delete useless and add template

---
+++ template/T_project/include/template.h
@@ -0,0 +1,20 @@
+
+/*****  define NAME  *****/
+#ifndef TEMPLATE_H
+# define TEMPLATE_H
+
+/***** includes *****/
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+/*****  struct  *****/
+typedef struct s_stack
+{
+	int				value;
+	struct s_stack	*next;
+}	t_stack;
+
+/*****  prototype  *****/
+
+
+#endif
